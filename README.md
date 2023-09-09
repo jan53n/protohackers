77c2b205b376d221b7f3200b8b227c4557e4c7f5
Jansen <jan53n@outlook.in>
2023-09-09 06:42:48 +0530

    add checklist of problems

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Server programming challenge.
 
 My solutions to the [Protohackers](https://protohackers.com/) challenge.
+
+
+## Problems
+
+* ✅ Smoke Test
+* ✅ Prime Time
+* ✅ Means to an End
+* ✅ Budget Chat
+* ✅ Unusual Database Program
+* ✅ Mob in the Middle
+* ❌ Speed Daemon
+* ❌ Line Reversal
+* ❌ Insecure Sockets Layer
+* ❌ Job Centre
+* ❌ Voracious Code Storage
+* ❌ Pest Control
--- speed_daemon/src/main.rs
+++ speed_daemon/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("[::]:5000")?;
+
+    fn handle_client(mut stream: TcpStream) {
+        let mut buf = [0];
+        stream.read_exact(&mut buf).unwrap();
+        println!("{:?}", buf[0]);
+        stream.write(&[0x10]).unwrap();
+    }
+
+    for stream in listener.incoming() {
+        let stream = stream?;
+        handle_client(stream);
+    }
+
+    Ok(())
 }
